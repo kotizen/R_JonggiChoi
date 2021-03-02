f293f154ed482d159ed5bd2d990289a5a876994f
kotizen <jkchoi0803@gmail.com>
2021-03-02 20:42:27 +0900

    First commit

--- Chapter_1.R
+++ Chapter_1.R
@@ -1,3 +1,23 @@
 
-# load pacakges #
+# load packages #
 # First start from define working directories #
+library(tidyverse)
+dt <- read.csv("ex_chb.csv")
+dat <- read.csv("ex_data.csv")
+
+colnames(dat)
+
+dat %>% 
+  count(b_eag)
+summary(dat$m6_inr)
+
+colnames(dat$b_alb)
+dat$b_alb
+
+temp <- dat %>% 
+  select(b_alt, b_alb, b_plt)
+
+temp %>% 
+  mutate(n1 = mean('b_alt', 'b_alb', 'b_plt'))
+
+         
